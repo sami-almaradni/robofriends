291b85724b22da5eff416dcee82e12ae2aa749da
samialmaradni97 <sami.almaradni97@gmail.com>
2023-02-11 16:59:32 +0100

    update the project to pwa

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
 
@@ -20,4 +21,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
     </Provider>
 );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
